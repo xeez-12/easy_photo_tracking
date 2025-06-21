4e32d132e07f137c6722232e38b1ac04cdbb5ca1
xeez-12 <fazrisment@gmail.com>
2025-06-22 05:45:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+// Hanya layani file statis untuk aset tertentu (misalnya CSS, JS, gambar)
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Rute utama langsung ke home.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'home.html'));
 });
+
+// Rute eksplisit ke index.html
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Tangani rute lain dengan redirect ke home
+app.get('*', (req, res) => {
+    res.redirect('/');
 });
 
 const PORT = process.env.PORT || 3000;
