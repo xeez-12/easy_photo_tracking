8b88955fe8f60f65d8433822074395503fc25125
xeez-12 <fazrisment@gmail.com>
2025-06-26 02:02:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname)));
 
 // Serve the index.html file for the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
