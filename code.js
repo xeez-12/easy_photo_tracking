cd04592248deb6e618c5a60333869182cd083170
xeez-12 <fazrisment@gmail.com>
2025-07-02 02:27:55 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,8 @@
+// Sample JavaScript code for the editor
+console.log("Hello, World!");
+function example() {
+    const message = "Welcome to the dark iOS-style code editor!";
+    console.log(message);
+    return message;
+}
+example();
